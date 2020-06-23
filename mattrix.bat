7000f7f9d4083c2dfc04d3ca19a7a0c7e68267fa
thesovietguy <62380529+thesovietguy@users.noreply.github.com>
2020-06-23 07:26:05 +0200

    Add files via upload

---
+++ mattrix.bat
@@ -0,0 +1,8 @@
+@echo off
+
+mode 10000
+color 0a
+
+:a
+echo %random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%
+goto a
---
+++ tool.bat
@@ -0,0 +1,26 @@
+@echo off
+color 0a
+title XHACKINGTOOLX
+echo :::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+echo 1.shutdown computers                         XHACKINGTOOLX
+echo 2.find ips
+echo 3.mattrix
+echo yt Meliodas    github thesovietguy               
+echo :::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+
+set /p _inputname= enter an input
+
+
+
+
+
+
+if %_inputname%==1 start shut.bat
+
+
+if %_inputname%==2 start net.bat
+
+if %_inputname%==3 start mattrix.bat
+
+
+
